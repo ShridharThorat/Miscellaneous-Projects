a4616bdb2952d41e8275f7ca083143ad65d2160e
Shridhar <shridhar.thorat01+github@gmail.com>
2023-11-13 19:46:09 +1000

    Added function to generate message for traveler and present attractions they might be interested in.

--- Computer Science Path/borderless-tourist/script.py
+++ Computer Science Path/borderless-tourist/script.py
@@ -56,20 +56,52 @@ print(attractions)
 # Finding the Best Places to Go
 
 def find_attractions(destination, interests):
+    '''
+    params:
+        destination that is a string describing city, country
+        interests is a list of strings describing places
+        
+    returns:
+        attractions in the city that include at least one of the interests
+    '''
     destination_index = get_destination_index(destination)
+    
+    # These the available attractions
     attractions_in_city = attractions[destination_index]
     
     attractions_with_interest = []
         
     for attraction in attractions_in_city:
-        possible_attraction = attraction
-        attraction_tags = attraction[1]
-        
+        # The first element in attraction is the name of the attraction
+        # And the second is an interest
+        attraction_tags = attraction[1]   
         for interest in interests:
             if interest in attraction_tags:
-                attractions_with_interest.append(possible_attraction[0])
+                attractions_with_interest.append(attraction[0])
 
     return attractions_with_interest
     
 la_arts = find_attractions("Los Angeles, USA",['art'])
 print(la_arts)
+
+# See The Parts of a City You want to See
+
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    
+    traveler_attractions = find_attractions(traveler_destination,
+                                            traveler_interests)
+    
+    reply = f"Hi {traveler[0]}, we think you'll like these places around {traveler_destination}: "
+    for attraction in traveler_attractions:
+        if attraction != traveler_attractions[-1]:
+            reply += attraction + ", "
+        else:
+            reply += attraction
+    reply += "."
+    return reply
+    
+derek = ['Dereck Smill', 'Paris, France', ['monument']]
+smills_france = get_attractions_for_traveler(derek)
+print(smills_france)
