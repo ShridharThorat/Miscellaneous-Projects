6108c1f25f93818a73f0821a5ca9d1f6e6cec244
Shridhar <shridhar.thorat01+github@gmail.com>
2023-11-21 11:21:01 +1000

    DFS

--- Computer Science Path/Trees and Graphs/tree.py
+++ Computer Science Path/Trees and Graphs/tree.py
@@ -16,7 +16,11 @@ class TreeNode:
         self.children = [child
                          for child in self.children
                          if child.value != child_node.value]
-
+        
+    def __repr__(self):
+        return self.value
+    
+    
     def traverse(self):
         print("Traversing...")
         nodes_to_visit = [self]
@@ -29,11 +33,19 @@ class TreeNode:
         stack = deque()
         stack.append([self, 0])
         level_str = "\n"
+        prev_level = 0
+        level = 0
         while len(stack) > 0:
+            prev_level = level
             node, level = stack.pop()
+            
+            if level > 0 and len(stack) > 0 and level <= stack[-1][1]:
+                level_str += "   "*(level-1)+ "├─"
+                
+            elif level > 0:
+                level_str += "   "*(level-1)+ "└─"
 
-            if level > 0:
-                level_str += "| "*(level-1) + "|-"
+
             level_str += str(node.value)
             level_str += "\n"
             level += 1
@@ -41,6 +53,17 @@ class TreeNode:
                 stack.append([child, level])
 
         return level_str
+    
+    def print_path(path):
+        # If path is None, no path was found
+        if path == None:
+            print("No paths found!")
+
+        # If a path was found, print path
+        else:  
+            print("Path found:")
+            for node in path:
+                print(node.value)
 
 
 # root = TreeNode("CEO")
@@ -52,4 +75,19 @@ class TreeNode:
 # root.add_child(second_child)
 # second_child.add_child(third_child)
 
+# print(root)
+
 # root.traverse()
+
+sample_root_node = TreeNode("A")
+two = TreeNode("B")
+three = TreeNode("C")
+sample_root_node.children = [three, two]
+four = TreeNode("D")
+five = TreeNode("E")
+six = TreeNode("F")
+seven = TreeNode("G")
+two.children = [five, four]
+three.children = [seven, six]
+
+# print(sample_root_node)
