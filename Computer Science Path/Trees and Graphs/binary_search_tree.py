ef32c3e51ad553ea8eaad81dcf3396b5a5f9d9da
Shridhar <shridhar.thorat01+github@gmail.com>
2023-11-21 15:36:24 +1000

    Binary search tree + heap init

---
+++ Computer Science Path/Trees and Graphs/binary_search_tree.py
@@ -0,0 +1,60 @@
+class BinarySearchTree:
+    def __init__(self, value, depth=1):
+        self.value = value
+        self.depth = depth
+        self.left = None
+        self.right = None
+        if depth == 1:
+            print(f"Tree node {self.value} added at depth {self.depth}")
+
+    def insert(self, value):
+        if (value < self.value):
+            if (self.left is None):
+                self.left = BinarySearchTree(value, self.depth + 1)
+                print(
+                    f'Tree node {value} added to the left of {self.value} at depth {self.depth + 1}')
+            else:
+                self.left.insert(value)
+        else:
+            if (self.right is None):
+                self.right = BinarySearchTree(value, self.depth + 1)
+                print(
+                    f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
+            else:
+                self.right.insert(value)
+
+    def get_node_by_value(self, value):
+        if (self.value == value):
+            return self
+        elif ((self.left is not None) and (value < self.value)):
+            return self.left.get_node_by_value(value)
+        elif ((self.right is not None) and (value >= self.value)):
+            return self.right.get_node_by_value(value)
+        else:
+            return None
+
+    # Define .depth_first_traversal() below:
+    # In order DFS: Acting on left -> current -> right
+    # Also a topological sort
+    def depth_first_traversal(self, path=[]):
+        if (self.left is not None):
+            self.left.depth_first_traversal()
+        print(f'Depth={self.depth}, Value={self.value}')
+        path.append(self.value)
+        if (self.right is not None):
+            self.right.depth_first_traversal()
+
+        return path
+
+
+tree = BinarySearchTree(48)
+tree.insert(24)
+tree.insert(55)
+tree.insert(26)
+tree.insert(38)
+tree.insert(56)
+tree.insert(74)
+
+# Print depth-first traversal:
+path = tree.depth_first_traversal()
+print(path)
